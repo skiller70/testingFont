c9333b0aeff531adbb6148a35136d741a0ce8627
skiller70 <singhlucky007007@gmail.com>
2024-01-11 11:38:21 +0530

    ggwp

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  distDir: "build",
+
   images: {
     domains: ["loremflickr.com"],
-  }
-}
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
